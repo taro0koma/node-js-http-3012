aaf7e821ab515fab6f400996bb9c9394c2a62735
YURI <taro0koma@gmail.com>
2022-04-19 16:16:51 +0900

    標準エラー出力の実装

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
-});
+        // infoとして標準出力する　ターミナルに表示する
+        console.info(`[${new Date()}] Requested by ${req.socket.remoteAddress}`);
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        });
+        res.write(req.headers['user-agent']);
+        res.end();
+    })
+    // createServerで作成したオブジェクトにonメソッドを接続する => {}).on('error'
+    // node index.js 2>&1 | tee -a application.log で標準出力と一緒に保存できる
+    .on('error', e => {
+        console.error(`[${new Date()}] Server Error`, e);
+    })
+    .on('clientError', e => {
+        console.error(`[${new Date()}] Client Error`, e);
+    });
 const port = 8000;
 server.listen(port, () => {
-  console.log(`Listening on ${port}`);
+    console.log(`Listening on ${port}`);
 });
